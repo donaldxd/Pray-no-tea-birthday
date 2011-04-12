9e4b0ab034de0dca06ea5345342a20f6a9a85871
Vishesh Handa <handa.vish@gmail.com>
2011-04-12 17:29:16 +0530

    Add some ground. Doesn't really work

--- js/appmanager.js
+++ js/appmanager.js
@@ -12,5 +12,17 @@ function AppManager() {
         
         var ball3 = new Ball( [600,100] );
         ball3.startUpBall();
+        
+        // Add some ground
+        var boxDef = new b2BoxDef();
+        boxDef.extents.Set( 600, 30 );
+        boxDef.restitution = 0.0;
+        
+        var groundBodyDef = new b2BodyDef();
+        groundBodyDef.AddShape( boxDef );
+        groundBodyDef.position.Set( 300, 300 );
+        
+        var ground = g_World.CreateBody( groundBodyDef );
+        console.log( ground );
     };
 }
