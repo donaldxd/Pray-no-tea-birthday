d7cbcca1d2f0afdf6ddb1facdfa49892d3ff9395
Vishesh Handa <handa.vish@gmail.com>
2011-04-14 04:41:14 +0530

    Better Friction and restitution

--- js/box.js
+++ js/box.js
@@ -18,8 +18,8 @@ function Box() {
         
         var fixture = new b2FixtureDef();
         fixture.density = 1.0;
-        fixture.restitution = 0.4;
-        fixture.friction = 0;
+        fixture.restitution = 0.25;
+        fixture.friction = 0.60;
         fixture.shape = new b2PolygonShape();
         fixture.shape.SetAsBox( w/PPM * 0.5, h/PPM * 0.5 );
         
--- js/player.js
+++ js/player.js
@@ -21,7 +21,7 @@ function Player() {
         var fixture = new b2FixtureDef();
         fixture.density = 1.0;
         fixture.restitution = 0.25;
-        fixture.friction = 0;
+        fixture.friction = 0.6;
         
         fixture.shape = new b2PolygonShape();
         fixture.shape.SetAsBox( this.width/PPM * 0.5, this.height/PPM * 0.5 );
