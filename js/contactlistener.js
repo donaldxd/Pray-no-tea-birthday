9cc32a2d749494b9baab66a1ac88427ba4b1439c
Vishesh Handa <handa.vish@gmail.com>
2011-04-14 12:30:34 +0530

    Better jumping based on contact normals

--- js/contactlistener.js
+++ js/contactlistener.js
@@ -5,10 +5,11 @@ function ContactListener() {
         var obj1 = contact.GetFixtureA().GetBody().GetUserData();
         var obj2 = contact.GetFixtureB().GetBody().GetUserData();
         
-        if( obj1 instanceof Player && obj2 instanceof Box ) {
+        var normal = getNormal( contact );
+        if( obj1 instanceof Player && normal.y > 0 ) {
             obj1.bCanJump = true;
         }
-        else if( obj2 instanceof Player && obj1 instanceof Box ) {
+        else if( obj2 instanceof Player && normal.y < 0 ) {
             obj2.bCanJump = true;
         }  
     };
@@ -16,14 +17,22 @@ function ContactListener() {
     this.EndContact = function( contact ) {
         var obj1 = contact.GetFixtureA().GetBody().GetUserData();
         var obj2 = contact.GetFixtureB().GetBody().GetUserData();
-        
-        if( obj1 instanceof Player && obj2 instanceof Box ) {
+
+        var normal = getNormal( contact );
+        if( obj1 instanceof Player && normal.y > 0 ) {
             obj1.bCanJump = false;
         }
-        else if( obj2 instanceof Player && obj1 instanceof Box ) {
+        else if( obj2 instanceof Player && normal.y < 0 ) {
             obj2.bCanJump = false;
         }  
     }
+    
+}
+
+function getNormal( contact ) {
+    var manifold = new Box2D.Collision.b2WorldManifold();
+    contact.GetWorldManifold( manifold );
+    return manifold.m_normal;
 }
 
 ContactListener.prototype = new Box2D.Dynamics.b2ContactListener();
