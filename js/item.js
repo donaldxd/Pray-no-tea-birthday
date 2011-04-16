4d57f67e29d9f0d7a4ef0a3a891a2a453a7002e7
Vishesh Handa <handa.vish@gmail.com>
2011-04-16 15:12:54 +0530

    Items now look like carrots

--- js/item.js
+++ js/item.js
@@ -9,9 +9,33 @@ function Item() {
     
     this.startUpItem = function( gs, x, y ) {
         this.gameScreen = gs;
-        this.radius = 10;
-        this.fillColor = "#eee";
-        this.startUpBall( x, y );        
+        
+        this.startUpImageObject( "img/carrot.png" );
+        this.width = this.image.width;
+        this.height = this.image.height;
+        
+        // Physics
+        var bodyDef = new b2BodyDef();
+        this.startPos = new b2Vec2( g2p(x), g2p(y) );
+        bodyDef.position.Set( g2p(x), g2p(y) );
+        bodyDef.type = b2Body.b2_dynamicBody; 
+        bodyDef.userData = this;
+        bodyDef.allowSleep = false;
+        bodyDef.fixedRotation = true;
+        
+        this.body = g_World.CreateBody(bodyDef);
+        
+        var fixture = new b2FixtureDef();
+        fixture.density = 1.0;
+        fixture.restitution = 0.25;
+        fixture.friction = 0.6;
+        
+        fixture.shape = new b2PolygonShape();
+        fixture.shape.SetAsBox( this.width/PPM * 0.5, this.height/PPM * 0.5 );
+        
+        this.body.CreateFixture( fixture );
+        
+        return this;
     }
     
     this.update = function( dt ) {
@@ -27,4 +51,4 @@ function Item() {
     };
 }
 
-Item.prototype = new Ball();
+Item.prototype = new ImageObject();
