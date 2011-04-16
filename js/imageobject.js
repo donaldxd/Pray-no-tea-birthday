2f47737d932aedb0819a974355cd99e819729a80
Vishesh Handa <handa.vish@gmail.com>
2011-04-16 17:23:21 +0530

    Better ImageObject handling
    It now works even if the object doesn't have a body

--- js/imageobject.js
+++ js/imageobject.js
@@ -6,18 +6,23 @@ function ImageObject() {
     
     this.image = new Image();
     
-    this.startUpImageObject = function( src ) {
-        this.startUpGameObject( 0 );
+    this.startUpImageObject = function( src, z) {
+        this.startUpGameObject( z );
         this.image.src = src;
         
         return this;
     }
     
     this.render = function( context ) {
-        var pos = this.body.GetPosition();
-        var x = p2cX( pos.x ) - this.image.width/2;
-        var y = p2cY( pos.y ) - this.image.height/2;
-        
+        if( this.body ) {
+            var pos = this.body.GetPosition();
+            var x = p2cX( pos.x ) - this.image.width/2;
+            var y = p2cY( pos.y ) - this.image.height/2;
+        }
+        else {
+            x = 0;
+            y = 0;
+        }
         context.drawImage( this.image, x, y );
     }
 }
