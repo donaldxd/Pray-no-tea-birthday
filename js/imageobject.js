6b20a0bdfe5381d01a8a33760395272d408104c6
Vishesh Handa <handa.vish@gmail.com>
2011-04-16 17:48:43 +0530

    Add createBody() in ImageObject
    Use it in the level changer

--- js/imageobject.js
+++ js/imageobject.js
@@ -25,6 +25,24 @@ function ImageObject() {
         }
         context.drawImage( this.image, x, y );
     }
+    
+    this.createBody = function( x, y ) {
+        var bodyDef = new b2BodyDef();
+        bodyDef.position.Set( g2p(x), g2p(y) );
+        bodyDef.type = b2Body.b2_staticBody;
+        bodyDef.userData = this;
+        
+        this.body = g_World.CreateBody(bodyDef);
+        
+        var fixture = new b2FixtureDef();
+        fixture.isSensor = true;
+        fixture.shape = new b2PolygonShape();
+        var w = this.image.width;
+        var h = this.image.height;
+        fixture.shape.SetAsBox( w/PPM * 0.5, h/PPM * 0.5 );
+        
+        this.body.CreateFixture( fixture );
+    }
 }
 
 ImageObject.prototype = new GameObject();
--- js/levelchanger.js
+++ js/levelchanger.js
@@ -13,22 +13,7 @@ function LevelChanger() {
         this.startUpGameObject( 0 );
         
         this.image.src = "img/door.png";
-        // Physics
-        var bodyDef = new b2BodyDef();
-        bodyDef.position.Set( g2p(x), g2p(y) );
-        bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.userData = this;
-        
-        this.body = g_World.CreateBody(bodyDef);
-        
-        var fixture = new b2FixtureDef();
-        fixture.isSensor = true;
-        fixture.shape = new b2PolygonShape();
-        var w = this.image.width;
-        var h = this.image.height;
-        fixture.shape.SetAsBox( w/PPM * 0.5, h/PPM * 0.5 );
-        
-        this.body.CreateFixture( fixture );
+        this.createBody( x, y );
         return this;
     }
     
